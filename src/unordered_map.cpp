a40cdcff4517dd27878dc9d0bdc94cd83d818abd
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-03-13 21:17:51 +0100

    Benchmark find on vector and unordered_map
    Results are suspicious because vector is always faster.

--- src/unordered_map.cpp
+++ src/unordered_map.cpp
@@ -1,5 +1,7 @@
 #include "benchmark/benchmark.h"
 
+#include <algorithm>
+#include <random>
 #include <unordered_map>
 
 static void BM_unordered_map_insert(benchmark::State& state)
@@ -7,8 +9,8 @@ static void BM_unordered_map_insert(benchmark::State& state)
     for (auto _ : state)
     {
         state.PauseTiming();
-        std::unordered_map<int, int> map;
         const int size = state.range(0);
+        std::unordered_map<int, int> map;
         state.ResumeTiming();
 
         for (int i = 0; i < size; ++i)
@@ -18,3 +20,48 @@ static void BM_unordered_map_insert(benchmark::State& state)
     }
 }
 BENCHMARK(BM_unordered_map_insert)->Range(1, 1e6);
+
+static void BM_unordered_map_reserve_and_insert(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        const int size = state.range(0);
+        std::unordered_map<int, int> map;
+        map.reserve(size);
+        state.ResumeTiming();
+
+        for (int i = 0; i < size; ++i)
+        {
+            map.insert({i, i});
+        }
+    }
+}
+BENCHMARK(BM_unordered_map_reserve_and_insert)->Range(1, 1e6);
+
+static void BM_unordered_map_find(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        const int size = state.range(0);
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < size; ++i)
+        {
+            map.insert({i, i});
+        }
+
+        std::random_device rd; // obtain a random number from hardware
+        std::mt19937 gen(rd()); // seed the generator
+        std::uniform_int_distribution<> distr(0, size - 1); // define the range
+        int item = distr(gen); // generate numbers
+        state.ResumeTiming();
+
+        auto it = map.find(item);
+        if (it == map.end() || it->first != item)
+        {
+            throw std::range_error("Element not found: " + std::to_string(item));
+        }
+    }
+}
+BENCHMARK(BM_unordered_map_find)->Range(1, 1e6);
--- src/vector.cpp
+++ src/vector.cpp
@@ -1,5 +1,7 @@
 #include "benchmark/benchmark.h"
 
+#include <algorithm>
+#include <random>
 #include <vector>
 
 static void BM_vector_push_back(benchmark::State& state)
@@ -7,8 +9,8 @@ static void BM_vector_push_back(benchmark::State& state)
     for (auto _ : state)
     {
         state.PauseTiming();
-        std::vector<int> vect;
         const int size = state.range(0);
+        std::vector<int> vect;
         state.ResumeTiming();
 
         for (int i = 0; i < size; ++i)
@@ -18,3 +20,48 @@ static void BM_vector_push_back(benchmark::State& state)
     }
 }
 BENCHMARK(BM_vector_push_back)->Range(1, 1e6);
+
+static void BM_vector_reserve_and_push_back(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        const int size = state.range(0);
+        std::vector<int> vect;
+        vect.reserve(size);
+        state.ResumeTiming();
+
+        for (int i = 0; i < size; ++i)
+        {
+            vect.push_back(i);
+        }
+    }
+}
+BENCHMARK(BM_vector_reserve_and_push_back)->Range(1, 1e6);
+
+static void BM_vector_find(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        const int size = state.range(0);
+        std::vector<int> vect;
+        for (int i = 0; i < size; ++i)
+        {
+            vect.push_back(i);
+        }
+
+        std::random_device rd; // obtain a random number from hardware
+        std::mt19937 gen(rd()); // seed the generator
+        std::uniform_int_distribution<> distr(0, size - 1); // define the range
+        int item = distr(gen); // generate numbers
+        state.ResumeTiming();
+
+        auto it = std::find(vect.begin(), vect.end(), item);
+        if (it == vect.end() || *it != item)
+        {
+            throw std::range_error("Element not found: " + std::to_string(item));
+        }
+    }
+}
+BENCHMARK(BM_vector_find)->Range(1, 1e6);
